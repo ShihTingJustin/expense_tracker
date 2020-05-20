1e329218d47d35779fc8748074842ad906e83591
Justin Huang <justinhuang777@gmail.com>
2020-05-20 11:14:18 +0800

    feat: add filter function

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,3 +7,37 @@ function submitCheck() {
 function deleteCheck() {
   return window.confirm('Do you really want to delete this record ?')
 }
+
+const selector = document.querySelector('.category-selector')
+const title = document.querySelector('.filter-title')
+const house = document.querySelector('.filter-house')
+const transport = document.querySelector('.filter-transport')
+const entertainment = document.querySelector('.filter-entertainment')
+const food = document.querySelector('.filter-food')
+const others = document.querySelector('.filter-others')
+
+const pathname = window.location.pathname
+if (pathname.includes('/filter/house')) {
+  selector.insertBefore(house, selector.firstChild)
+  title.remove()
+}
+
+if (pathname.includes('/filter/transport')) {
+  selector.insertBefore(transport, selector.firstChild)
+  title.remove()
+}
+
+if (pathname.includes('/filter/entertainment')) {
+  selector.insertBefore(entertainment, selector.firstChild)
+  title.remove()
+}
+
+if (pathname.includes('/filter/food')) {
+  selector.insertBefore(food, selector.firstChild)
+  title.remove()
+}
+
+if (pathname.includes('/filter/others')) {
+  selector.insertBefore(others, selector.firstChild)
+  title.remove()
+}
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,10 +4,22 @@ const router = express.Router()
 const Record = require('../../models/record')
 
 router.get('/', (req, res) => {
-  Record.find()
+  return Record.find()
+    .lean()
+    .then(records => {
+      let totalAmount = 0
+      records.forEach(record => totalAmount += Number(record.amount))
+      res.render('index', { records, totalAmount })
+    })
+    .catch(error => console.error(error))
+})
+
+router.get('/filter/:category', (req, res) => {
+  const { category } = req.params
+  return Record.find({ category: category })
     .lean()
     .then(records => res.render('index', { records }))
-    .catch(error => console.error(error))
+    .catch(error => console.log(error))
 })
 
 module.exports = router
