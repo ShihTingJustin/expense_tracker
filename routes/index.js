5ea59914424005bf4b959f6c2f6b58e0dc9882cc
Justin Huang <justinhuang777@gmail.com>
2020-06-12 10:53:23 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ const router = express.Router()
 const home = require('./modules/home')
 const records = require('./modules/records')
 const filter = require('./modules/filter')
+const users = require('./modules/users')
 
-
+router.use('/u', users)
 router.use('/records', records)
 router.use('/f', filter)
 router.use('/', home)
