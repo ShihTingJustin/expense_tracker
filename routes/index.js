b66196ecec15fcf356688e12254ed904bfe71807
Justin Huang <justinhuang777@gmail.com>
2020-06-12 15:31:56 +0800

    feat: add user authentication in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const { authenticator } = require('../middleware/auth')
 
 const home = require('./modules/home')
 const records = require('./modules/records')
 const filter = require('./modules/filter')
 const users = require('./modules/users')
 
-router.use('/records', records)
+router.use('/records', authenticator, records)
 router.use('/u', users)
-router.use('/f', filter)
-router.use('/', home)
+router.use('/f', authenticator, filter)
+router.use('/', authenticator, home)
 
 module.exports = router
