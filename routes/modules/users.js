a0c2dc32866fa0cb23f313cf1f13c040e3515794
Justin Huang <justinhuang777@gmail.com>
2020-06-12 11:21:55 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,4 +5,12 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {
+  
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
 module.exports = router
