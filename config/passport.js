0bcfc829745450b537e15780a43bb018e5cd3d8f
Justin Huang <justinhuang777@gmail.com>
2020-06-12 19:24:18 +0800

    feat: add bcrypt to login and register route

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
 
 const User = require('../models/user')
 
@@ -9,18 +10,21 @@ module.exports = app => {
   app.use(passport.session())
 
   passport.use(new LocalStrategy(
-    { usernameField: 'email', passReqToCallback: true  }, (req, email, password, done) => {
+    { usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
       User.findOne({ email })
         .then(user => {
-          if (!user) {      
+          if (!user) {
             req.flash('error_msg', 'This email is not registered.')
             return done(null, false)
           }
-          if (user.password !== password) {
-            req.flash('error_msg', 'Password incorrect.')
-            return done(null, false)
-          }
-          return done(null, user)
+          return bcrypt.compare(password, user.password).then(isMatch => {
+            if (!isMatch) {
+              req.flash('error_msg', 'Password incorrect.')
+              return done(null, false)
+            }
+            return done(null, user)
+          })
+            .catch(err => console.log(`[Error]:${err}`))
         })
     }
   ))
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 const router = express.Router()
 
 const User = require('../../models/user')
@@ -69,11 +70,15 @@ router.post('/register', (req, res) => {
         })
       } else {
         // create data and redirect
-        return User.create({
-          name,
-          email,
-          password
-        })
+        return bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(password, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          })
+          )
           .then(() => res.redirect('/'))
           .catch(err => console.log(`[Error]:${err}`))
       }
