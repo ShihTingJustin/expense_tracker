db264211a7bf099439162f0898f65b214bb4e12b
Justin Huang <justinhuang777@gmail.com>
2020-06-12 11:41:37 +0800

    feat: add register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -6,11 +6,15 @@ router.get('/login', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
-  
+
 })
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+
 })
 
 module.exports = router
